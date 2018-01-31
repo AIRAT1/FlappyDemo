73b8738b6f91792fd49b03843378c3a3a95bef7f
Ayrat <ayrat1@mail.ru>
2018-01-31 10:41:07 +0300

    v8 complete

--- core/src/de/android/ayrathairullin/sprites/Bird.java
+++ core/src/de/android/ayrathairullin/sprites/Bird.java
@@ -5,27 +5,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
 
 public class Bird {
-    public static final int GRAVITY = - 15;
+    private static final int MOVEMENT = 100;
+    private static final int GRAVITY = -15;
+    private Vector3 position;
+    private Vector3 velosity;
 
-    private Vector3 position, velosity;
     private Texture bird;
 
-    public Bird(int x, int y) {
+    public Bird(int x, int y){
         position = new Vector3(x, y, 0);
         velosity = new Vector3(0, 0, 0);
         bird = new Texture("bird.png");
-    }
-
-    public void update(float dt) {
-        if (position.y > 0) {
-            velosity.add(0, GRAVITY, 0);
-        }
-        velosity.scl(dt);
-        position.add(0, velosity.y, 0);
-
-        if (position.y < 0) position.y = 0;
-
-        velosity.scl(1 / dt);
     }
 
     public Vector3 getPosition() {
@@ -36,7 +26,19 @@ public class Bird {
         return bird;
     }
 
-    public void jump() {
+    public void update(float dt){
+        if (position.y > 0)
+            velosity.add(0, GRAVITY, 0);
+        velosity.scl(dt);
+        position.add(MOVEMENT * dt, velosity.y, 0);
+        if (position.y < 0)
+            position.y = 0;
+
+        velosity.scl(1 / dt);
+
+
+    }
+    public void jump(){
         velosity.y = 250;
     }
 }
--- core/src/de/android/ayrathairullin/states/PlayState.java
+++ core/src/de/android/ayrathairullin/states/PlayState.java
@@ -4,30 +4,55 @@ package de.android.ayrathairullin.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 
 import de.android.ayrathairullin.FlappyDemo;
 import de.android.ayrathairullin.sprites.Bird;
+import de.android.ayrathairullin.sprites.Tube;
 
-public class PlayState extends State{
+public class PlayState extends State {
+
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+
     private Bird bird;
     private Texture bg;
+
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
-        bg = new Texture("bg.png");
         camera.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
+
+        tubes = new Array<Tube>();
+
+        for (int i = 0; i < TUBE_COUNT; i++){
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
     protected void handleInput() {
-        if (Gdx.input.justTouched()) bird.jump();
+        if (Gdx.input.justTouched())
+            bird.jump();
+
     }
 
     @Override
     public void update(float dt) {
         handleInput();
         bird.update(dt);
+        camera.position.x = bird.getPosition().x + 80;
+
+        for (Tube tube : tubes){
+            if (camera.position.x - (camera.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+        camera.update();
+
     }
 
     @Override
@@ -36,7 +61,12 @@ public class PlayState extends State{
         sb.begin();
         sb.draw(bg, camera.position.x - (camera.viewportWidth / 2), 0);
         sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        for (Tube tube : tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosBotTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
         sb.end();
+
     }
 
     @Override
