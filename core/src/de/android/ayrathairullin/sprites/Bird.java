ac75b568ace3537fc6cdf47c9034d95b50989e95
Ayrat <ayrat1@mail.ru>
2018-01-31 22:02:04 +0300

    v11 complete

--- core/src/de/android/ayrathairullin/sprites/Bird.java
+++ core/src/de/android/ayrathairullin/sprites/Bird.java
@@ -2,6 +2,7 @@ package de.android.ayrathairullin.sprites;
 
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
@@ -11,25 +12,28 @@ public class Bird {
     private Vector3 position;
     private Vector3 velosity;
 
-    private Texture bird;
     private Rectangle bounds;
+    private Texture texture;
+    private Animation birdAnimation;
 
     public Bird(int x, int y){
         position = new Vector3(x, y, 0);
         velosity = new Vector3(0, 0, 0);
-        bird = new Texture("bird.png");
-        bounds = new Rectangle(x, y, bird.getWidth(), bird.getHeight());
+        texture = new Texture("birdanimation.png");
+        birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
     }
 
     public Vector3 getPosition() {
         return position;
     }
 
-    public Texture getBird() {
-        return bird;
+    public TextureRegion getBird() {
+        return birdAnimation.getFrame();
     }
 
     public void update(float dt){
+        birdAnimation.update(dt);
         if (position.y > 0)
             velosity.add(0, GRAVITY, 0);
         velosity.scl(dt);
@@ -50,6 +54,6 @@ public class Bird {
     }
 
     public void dispose() {
-        bird.dispose();
+        texture.dispose();
     }
 }
